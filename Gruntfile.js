aee4c4bb1e6b2d3a6c55fe9446e458e10d98b1e6
MichaelArestad <marestad@gmail.com>
2014-08-06 11:36:43 +0100

    ULTIMATE AMOTIC COMMIT
    Yes. AMOTIC.
    
    * Seriously. Changed file structure dramatically. Not Batman
    dramatically, but still pretty up there.
    * Added a fancy section mixin that lets sections auto-number (but not
    the top Table of Contents… wah wah)
    * Removed some dumb variables and maps (they were overkill)
    * added `_utilities` folder (super cool place for things like clearfix
    and normalize)
    * Decided a section for IE was illogical. Preferred method is now
    including IE-specific classes with respective element being IE-ized.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,61 +1,65 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	// 1. All configuration goes here
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
 
-        sass: {
-            dist: {
-                options: {
-                    // Can be nested, compact, compressed, expanded
-                    style: 'expanded'
-                },
-                files: {
-                    'style.css': 'scss/style.scss',
-                    'rtl.css': 'scss/rtl.scss',
-                    'css/editor-style.css': 'scss/editor-style.scss',
-                }
-            }
-        },
-        autoprefixer: {
-            options: {
-                // Task-specific options go here.
-            },
-            global: {
-                options: {
-                    // Target-specific options go here.
-                    // browser-specific info: https://github.com/ai/autoprefixer#browsers
-                    // DEFAULT: browsers: ['> 1%', 'last 2 versions', 'ff 17', 'opera 12.1']
-                    browsers: ['> 1%', 'last 2 versions', 'ff 17', 'opera 12.1', 'ie 8', 'ie 9']
-                },
-                src: 'css/style.css'
-            },
-        },
-        watch: {
-            scss: {
-                files: ['scss/*.scss', 'scss/**/*.scss', 'scss/**/**/*.scss'],
-                tasks: ['sass'],
-                options: {
-                    spawn: false,
-                }
-            },
-            autoprefixer: {
-                files: ['*.css', '**/*.css',],
-                tasks: ['autoprefixer'],
-                options: {
-                    livereload: true,
-                    spawn: false,
-                }
-            }
-        }
-    });
+		sass: {
+			dist: {
+				options: {
+					// Can be nested, compact, compressed, expanded
+					style: 'expanded'
+				},
+				files: {
+					'style.css': 'scss/style.scss',
+					'rtl.css': 'scss/rtl.scss',
+					'css/editor-style.css': 'scss/editor-style.scss',
+				}
+			}
+		},
+		autoprefixer: {
+			options: {
+				// Task-specific options go here.
+			},
+			global: {
+				options: {
+					// Target-specific options go here.
+					// browser-specific info: https://github.com/ai/autoprefixer#browsers
+					// DEFAULT: browsers: ['> 1%', 'last 2 versions', 'ff 17', 'opera 12.1']
+					browsers: ['> 1%', 'last 2 versions', 'ff 17', 'opera 12.1', 'ie 8', 'ie 9']
+				},
+				src: 'css/style.css'
+			},
+		},
+		watch: {
+			scss: {
+				files: [
+					'scss/*.scss',
+					'scss/**/*.scss',
+					'scss/**/**/*.scss'
+				],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				}
+			},
+			autoprefixer: {
+				files: ['*.css', '**/*.css',],
+				tasks: ['autoprefixer'],
+				options: {
+					livereload: true,
+					spawn: false,
+				}
+			}
+		}
+	});
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
-    grunt.loadNpmTasks('grunt-contrib-watch');      // watch files for changes
-    grunt.loadNpmTasks('grunt-contrib-sass');       // Gettin Sassy!
-    grunt.loadNpmTasks('grunt-autoprefixer');       // Auto-freaking-prefixer!!!
+	// 3. Where we tell Grunt we plan to use this plug-in.
+	grunt.loadNpmTasks('grunt-contrib-watch');      // watch files for changes
+	grunt.loadNpmTasks('grunt-contrib-sass');       // Gettin Sassy!
+	grunt.loadNpmTasks('grunt-autoprefixer');       // Auto-freaking-prefixer!!!
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['watch']);
+	// 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+	grunt.registerTask('default', ['watch']);
 
 };
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 th_s (THUNDERSCORES)
 ===
 
-Hi! I'm a set of Sass files called `th_s` or `THUNDERSCORES` meant to be used within the [_s](http://underscores.me/ "So good!") WordPress starter theme, but you could easily repurpose it for pretty much whatever you want.
+Hi! I'm a set of Sass files called `th_s` or `THUNDERSCORES` meant to be used within the [_s](http://underscores.me/ "So good!") WordPress starter theme, but you could easily repurpose it for pretty much whatever you want. Pretty much.
 
 Some cool stuff about it:
-* For now it requires Sass v3.3.0 Maptastic Maple or newer.
-* All your files are super duper organized.
+* For now it requires Sass v3.3.0 Maptastic Maple or newer (BECAUSE MAPS!).
+* All your files are super duper organized via Atomic folder structure.
 * Mixins, extends, functions, and variable files are grouped with related modules. This makes your code a bit more maintainable later down the road.
 * It's got some generated editor styles! Time-saver.
 * Licensed under GPLv2 or later. :) Use it to make something cool.
@@ -16,7 +16,7 @@ Getting Started
 The simplest way to get going is to download `th_s` from Github. Then head over to http://underscores.me to generate your `_s`-based theme. Follow these steps to combine them:
 
 1. Move the contents of `th_s` into yout newly generated theme directory.
-2. Then, update the stylesheet header in `style.scss`.
+2. Then, update the comment block in `style.scss` to reflect your new theme.
 3. You can process the files using your favorite compiler. CodeKit and Grunt are my personal favorites.
 4. Update or delete this readme.
 
@@ -33,14 +33,7 @@ Then run `grunt` to start watching and compiling your CSS files.
 You may also have to install Sass. It's as easy as running `gem install sass`.
 
 
-#### Grunt CLI
-
-If you have any issues with running the `grunt` command then install grunt-cli globally using:
-
-`sudo npm install -g grunt-cli`
-
 ### Cool. Coolcoolcool.
-
 
 Now you're ready to go! The next step is easy to say, but harder to do: make an awesome WordPress theme. :)
 
